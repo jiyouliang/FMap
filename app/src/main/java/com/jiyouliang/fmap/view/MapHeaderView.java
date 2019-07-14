9d66106e072e86b37bcaba091111efcea25cb8f3
jiyouliang <jeyouliang@gmail.com>
2019-07-14 14:33:46 +0800

    修改MapHeaderView继承RelativeLayout

--- app/src/main/java/com/jiyouliang/fmap/view/MapHeaderView.java
+++ app/src/main/java/com/jiyouliang/fmap/view/MapHeaderView.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.jiyouliang.fmap.R;
 
 /**
  * 头部
  */
-public class MapHeaderView extends ConstraintLayout {
+public class MapHeaderView extends RelativeLayout {
+    private OnMapHeaderViewClickListener mListener;
+    private ImageView mIvUser;
+
     public MapHeaderView(Context context) {
         this(context, null);
     }
@@ -25,5 +30,37 @@ public class MapHeaderView extends ConstraintLayout {
                 .inflate(R.layout.view_map_header, this, true);
 
 
+        mIvUser = findViewById(R.id.iv_user);
+    }
+
+    public void setOnMapHeaderViewClickListener(OnMapHeaderViewClickListener listener){
+        if(listener != null){
+            this.mListener = listener;
+        }
+    }
+
+    /**
+     * MapHeaderView点击监听
+     */
+    public interface OnMapHeaderViewClickListener{
+        /**
+         * 点击用户
+         */
+        void onUserClick();
+
+        /**
+         * 点击搜索
+         */
+        void onSearchClick();
+
+        /**
+         * 点击语音识别
+         */
+        void onVoiceClick();
+
+        /**
+         * 点击二维码扫描
+         */
+        void onQrScanClick();
     }
 }
