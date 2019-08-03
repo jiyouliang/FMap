cd1f50a4fc71f99df3328e84623c42cdb11cd174
jiyouliang <jeyouliang@gmail.com>
2019-08-03 18:34:39 +0800

    fix issue#1

---
+++ app/src/main/java/com/jiyouliang/fmap/ui/user/UserDetailActivity.java
@@ -0,0 +1,18 @@
+package com.jiyouliang.fmap.ui.user;
+
+import android.os.Bundle;
+
+import com.jiyouliang.fmap.R;
+import com.jiyouliang.fmap.ui.BaseActivity;
+
+/**
+ * 用户详情页
+ */
+public class UserDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detail);
+    }
+}
